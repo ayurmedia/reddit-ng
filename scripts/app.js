30b770d26bb50b8a43209d09d8230d7acaf1686e
Daniel Unterberger <daniel@ayurmedia.de>
2013-08-30 10:29:27 +0200

    html mode

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,7 @@ var appModule = angular.module('redditApp', [])
 	
 	// if using html5Mode(true) you MUST use full urls
 	// in HashBang-Mode it also works with prefixes in path
-	$locationProvider.html5Mode(true);
+	//$locationProvider.html5Mode(true);
     $routeProvider
       .when('/', {
         templateUrl: 'views/comments.html',

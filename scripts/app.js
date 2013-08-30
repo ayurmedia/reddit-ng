5761e060c4cc779afe724e6a0f766828080fb221
Daniel Unterberger <daniel@ayurmedia.de>
2013-08-30 10:30:30 +0200

    path

--- scripts/app.js
+++ scripts/app.js
@@ -5,13 +5,13 @@ var appModule = angular.module('redditApp', [])
 	
 	// if using html5Mode(true) you MUST use full urls
 	// in HashBang-Mode it also works with prefixes in path
-	//$locationProvider.html5Mode(true);
+	$locationProvider.html5Mode(true);
     $routeProvider
-      .when('/', {
+      .when('/reddit-ng/', {
         templateUrl: 'views/comments.html',
         controller: 'CommentCtrl'
       })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/reddit-ng/'
       });
   });
